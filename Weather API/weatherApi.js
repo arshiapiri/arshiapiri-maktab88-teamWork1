174191b89af66f40b88b856e9d8d2c9b1ee5f859
arshiapiri <arshia.piri8585@gmail.com>
2023-01-23 15:44:48 +0330

    update

--- Weather API/weatherApi.js
+++ Weather API/weatherApi.js
@@ -1,13 +1,20 @@
+const selected = document.getElementById('selected');
+const capitals = document.getElementById('capital');
 let countries=[];
+let capital=[];
 $.ajax({
     url:"https://restcountries.com/v3.1/all",
     type:"GET",
     dataType: "json",
     async:false,
     success: function(res){
-        for(let i=0;i<res.length; i++){
+        for(let i=0; i<res.length; i++){
             countries.push(res[i]["name"]["common"]);
-        }  
+            capital.push(res[i]["capital"])
+            $("#capital").html(`
+                <p>capital:${capital[0]}</p>`
+            )
+        } 
     },
     error: function(err){
         console.log(err);
@@ -21,20 +28,13 @@ function createOptions(){
     }
     return optionBody;
 }
-document.getElementById('selected').innerHTML="<option selected>--- Choose  Country ---</option>" +createOptions();
+selected.innerHTML="<option selected>--- Choose  Country ---</option>" +createOptions();
 
-const mapBody = document.getElementById('mapBody');
-const map = () => {
-    mapBody.innerHTML = `           
-    <div class="col-lg-9 col-md-6 col-12">
-    <div class="card shadow">
-        <div class="mapouter">
-            <iframe width="100%" height="250" id="gmap_canvas" 
-                src="https://maps.google.com/maps?q=tehran=&z=13&ie=UTF8&iwloc=&output=embed"
-                frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-            </iframe>
-        </div>
-    </div>
-</div>
-`
-}
+// function capitalCountry (){
+//     let capitalBody = '';
+//     for (let i =0;i<capitals.length;i++) {
+//         capitalBody += `<p>capital:${capitals[1]}</p>` 
+//     }
+//     return capitalBody;
+// }
+// capital.innerHTML = "<p>capital:--</p>" +capitalCountry();
