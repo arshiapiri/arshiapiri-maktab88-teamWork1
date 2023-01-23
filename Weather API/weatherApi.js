065d80d73c19a9a36bf048d22d41663f85e6aa7b
arshiapiri <arshia.piri8585@gmail.com>
2023-01-23 13:57:23 +0330

    update

--- Weather API/weatherApi.js
+++ Weather API/weatherApi.js
@@ -17,11 +17,24 @@ $.ajax({
 function createOptions(){
     let optionBody='';
     for(let i=0;i<countries.length;i++){
-    optionBody+= `<option value="1">${countries[i]}</option>`;
-
+    optionBody+= `<option value="${[i+1]}">${countries[i]}</option>`;
     }
     return optionBody;
-
 }
-
 document.getElementById('selected').innerHTML="<option selected>--- Choose  Country ---</option>" +createOptions();
+
+const mapBody = document.getElementById('mapBody');
+const map = () => {
+    mapBody.innerHTML = `           
+    <div class="col-lg-9 col-md-6 col-12">
+    <div class="card shadow">
+        <div class="mapouter">
+            <iframe width="100%" height="250" id="gmap_canvas" 
+                src="https://maps.google.com/maps?q=tehran=&z=13&ie=UTF8&iwloc=&output=embed"
+                frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+        </div>
+    </div>
+</div>
+`
+}
